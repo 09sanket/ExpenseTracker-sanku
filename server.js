f212a99d3e2dc07e542e223ff691f189c9152579
sanket rahangdale <sanketrahangdale1118@gmail.com>
2023-12-08 13:59:57 +0530

    login page

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const db = require('./database');
+
+// Body parser middleware to handle form data
+app.use(express.urlencoded({ extended: false }));
+
+// Serve static files (like HTML, CSS)
+app.use(express.static('public'));
+
+
+
+
+
+// Handle form submission
+app.post('/signup', (req, res) => {
+  const { username, email, password } = req.body;
+
+  const INSERT_USER_QUERY = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+
+  db.query(INSERT_USER_QUERY, [username, email, password], (err, result) => {
+    if (err) {
+      res.status(500).send('Error inserting user');
+    } else {
+      res.status(200).send('User inserted successfully');
+    }
+  });
+});
+
+module.exports = app;
